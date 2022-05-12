6aeee75cafd2c1a00d7ace5dcfd5cf5050faf3c7
Andy Roxby <aroxby@users.noreply.github.com>
2022-05-12 11:38:03 -0400

    Add omitted dragon type

--- pokemon_data.py
+++ pokemon_data.py
@@ -24,6 +24,7 @@ class Type(Enum):
         MAPPING = {
             self.BIRD: 'Bird',
             self.BUG: 'Bug',
+            self.DRAGON: 'Dragon',
             self.ELECTRIC: 'Electric',
             self.FIGHTING: 'Fighting',
             self.FIRE: 'Fire',
